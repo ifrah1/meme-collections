5e86cffbd98c5883f78d46436251639fe9e5744e
ifrah1 <arifrah125@gmail.com>
2021-01-30 17:30:06 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/* External Modules */
+const express = require('express');
+const logger = require('morgan');
+
+/* Internal Modules*/
+
+const PORT = 3000;  // for now local, later will change to .env file
+
+/* Set up Express App */
+const app = express();
+
+/* Config app */
+app.set('view engine', 'ejs');
+
+/* Mount middleware */
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* Routes */
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+/* App listener */
+app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
