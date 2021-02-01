7e19aa6f58e04958ff6cdc2fdc1fea9d6dab2458
ifrah1 <arifrah125@gmail.com>
2021-02-01 14:26:57 -0500

    started oAuth setup, stopped at step 6

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 /* External Modules */
 const express = require('express');
 const logger = require('morgan');
+//used for handling for oAuth
+const session = require('express-session');
+const passport = require('passport');
 
 /* Internal Modules*/
+
+const { memesRoute } = require('./routes');
 
 const PORT = 3000;  // for now local, later will change to .env file
 
@@ -11,6 +16,8 @@ const app = express();
 
 /* Connect to MongoDB*/
 require('./config/database');
+//require passport 
+require('./config/passport');
 
 /* Config app */
 app.set('view engine', 'ejs');
@@ -20,11 +27,18 @@ app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// session handling 
+app.use(session({
+    secret: 'ArifMemeApp', //change later
+    resave: false,
+    saveUninitialized: true
+}));
+// used for handling oAuth
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* Routes */
-app.get('/', (req, res) => {
-    res.send('Hello');
-})
+app.use('/', memesRoute);
 
 /* App listener */
 app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
