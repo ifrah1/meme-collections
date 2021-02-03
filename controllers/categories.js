98ed5010412668f2bc32fc3dc939a7f52d5cdeaa
ifrah1 <arifrah125@gmail.com>
2021-02-03 00:25:37 -0500

    added code to make sure categorry already does not exist

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,7 +3,8 @@ const { Category } = require('../models');
 const showNew = (req, res) => {
     // console.log(req.user); //shows logged user info
     res.render('category/newCat', {
-        user: req.user
+        user: req.user,
+        exist: false
     });
 }
 
@@ -11,24 +12,33 @@ const create = (req, res) => {
     // console.log(req.user); //shows logged user info
     console.log(req.body)
 
-    const category = new Category(req.body);
-
-    //using mongoose save function to save to DB
-    category.save(err => {
-        // exit if err happens in DB storing the object
-        if (err) {
-            console.log(err);
+    Category.findOne({ 'name': req.body.name }, (err, category) => {
+        if (err) return console.log(err);
+        if (category) {
             return res.render('category/newCat', {
-                user: req.user
+                user: req.user,
+                exist: true
+            });
+        } else {
+            const category = new Category(req.body);
+
+            //using mongoose save function to save to DB
+            category.save(err => {
+                // exit if err happens in DB storing the object
+                if (err) {
+                    console.log(err);
+                    return res.render('category/newCat', {
+                        user: req.user,
+                        exist: false
+                    });
+                }
+                res.redirect('/'); //redirect to main page for now
+
             });
         }
-
-        res.redirect('/'); //redirect to main page for now
-
     });
 
 }
-
 
 
 module.exports = {
