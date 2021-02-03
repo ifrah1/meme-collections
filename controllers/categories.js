af77aa39c08a3efbdcb52846b398c73bdc4bcb16
ifrah1 <arifrah125@gmail.com>
2021-02-03 00:07:55 -0500

    creates new category and stores in db

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,3 +1,5 @@
+const { Category } = require('../models');
+
 const showNew = (req, res) => {
     // console.log(req.user); //shows logged user info
     res.render('category/newCat', {
@@ -5,7 +7,31 @@ const showNew = (req, res) => {
     });
 }
 
+const create = (req, res) => {
+    // console.log(req.user); //shows logged user info
+    console.log(req.body)
+
+    const category = new Category(req.body);
+
+    //using mongoose save function to save to DB
+    category.save(err => {
+        // exit if err happens in DB storing the object
+        if (err) {
+            console.log(err);
+            return res.render('category/newCat', {
+                user: req.user
+            });
+        }
+
+        res.redirect('/'); //redirect to main page for now
+
+    });
+
+}
+
+
 
 module.exports = {
     showNew,
+    create
 }
--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,11 @@ const { categoryCtrl } = require('../controllers');
 // GET app.com/category/new 
 router.get('/new', isLoggedIn, categoryCtrl.showNew);
 
+// POST app.com/category/new 
+router.post('/new', isLoggedIn, categoryCtrl.create);
+
+
+
 // Insert this middleware for routes that require a logged in user
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
